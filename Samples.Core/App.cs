4e4055d0031ca8e25f83bfe5b0a4a5914c00bc6b
Allan Ritchie <allan.ritchie@gmail.com>
2018-10-11 21:05:03 -0400

    sample fixes

--- Samples.Core/App.cs
+++ Samples.Core/App.cs
@@ -10,6 +10,7 @@ namespace Samples
         public App()
         {
             this.MainPage = new NavigationPage(new StartPage());
+            GlobalExceptionHandler.Register();
         }
     }
 }
---
+++ Samples.Core/GlobalExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Acr.UserDialogs;
+using ReactiveUI;
+
+
+namespace Samples
+{
+    public class GlobalExceptionHandler : IObserver<Exception>
+    {
+        public static void Register() => RxApp.DefaultExceptionHandler = new GlobalExceptionHandler();
+        public void OnCompleted() {}
+        public void OnError(Exception error) {}
+
+
+        public void OnNext(Exception value)
+        {
+            Console.WriteLine(value);
+            UserDialogs.Instance.Alert(value.ToString());
+        }
+    }
+}
